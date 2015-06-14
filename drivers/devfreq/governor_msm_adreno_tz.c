9f05b62ec03f901f05812292de33cdae28044858
DespairFactor <matthewalex@outlook.com>
2015-06-14 12:37:39 -0400

    adreno_idler: remove power_suspended variable, we don't use it
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -145,7 +145,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	 * Force to use & record as min freq when system has
 	 * entered pm-suspend or screen-off state.
 	 */
-	if (suspended || power_suspended) {
+	if (suspended) {
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 0;
 	}
