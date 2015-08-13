f8104669fe1e2a94c0aa8b1b8777c5129bb8742c
ZaneZam <cyxman@yahoo.com>
2015-08-13 18:29:09 +0200

    cpufreq: zzmoove: Fix lcd notifier and powersuspend compile issues - added missing part!
    thx to @777jon for pointing!
    
    Signed-off-by: randomlyjobless&bored <randomlybored@openmailbox.org>

--- drivers/cpufreq/cpufreq_zzmoove.c
+++ drivers/cpufreq/cpufreq_zzmoove.c
@@ -7828,7 +7828,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		   &dbs_attr_group);
 #if defined(CONFIG_HAS_EARLYSUSPEND) && !defined (DISABLE_POWER_MANAGEMENT)
 		unregister_early_suspend(&_powersave_early_suspend);
-#elif defined(CONFIG_POWERSUSPEND) && !defined (DISABLE_POWER_MANAGEMENT)
+#elif defined(CONFIG_POWERSUSPEND) && !defined(USE_LCD_NOTIFIER) && !defined(DISABLE_POWER_MANAGEMENT) || defined(CONFIG_POWERSUSPEND) && defined(USE_LCD_NOTIFIER) && !defined(DISABLE_POWER_MANAGEMENT)
 		if (cpu == 0)
 		    unregister_power_suspend(&powersave_powersuspend);
 #endif
